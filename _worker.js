c5e8dd8da64aab0cd3bd056b611a2700d5e68276
145a <133004095+54145a@users.noreply.github.com>
2023-10-03 17:42:48 +0800

    try

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    url.hostname = "github.com";
+    url.hostname = url.pathname.includes("session")?"kgithub.com":"github.com";
     let new_request = new Request(url, request);
     return fetch(new_request);
   }
