4bb2cb6e1058fc329b1a81fc740dbeb5b01391c0
54145a <133004095+54145a@users.noreply.github.com>
2023-10-03 06:39:30 +0000

    666

--- _worker.js
+++ _worker.js
@@ -1,8 +1,27 @@
+const rewriter = new HTMLRewriter()
+  .on('a', new AttributeRewriter('href'))
+  //.on('img', new AttributeRewriter('src'))
+ 
+class AttributeRewriter {
+  constructor(attributeName) {
+    this.attributeName = attributeName
+  }
+  element(element) {
+    const attribute = element.getAttribute(this.attributeName)
+    if (attribute) {
+      element.setAttribute(
+        this.attributeName,
+        attribute.replace('github.com', '145atest2.pages.dev')
+      )
+    }
+  }
+}
 export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
     _url.hostname = "github.com";
-    const req = new Request(_url, request);
-    return fetch(req);
+    const req = await fetch(new Request(_url, request));
+  const res = await fetch(req);
+  return rewriter.transform(res);
   },
 };
