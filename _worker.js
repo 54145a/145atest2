67e274a6e59264a09e82e308bb36249ac570cfcb
145a <133004095+54145a@users.noreply.github.com>
2023-10-04 21:06:59 +0800

    gh!!

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
       let url = new URL(request.url);
-      url.hostname = "huggingface.co";
+      url.hostname = "github.com";
       let new_request = new Request(url, request);
       return fetch(new_request);
   }
