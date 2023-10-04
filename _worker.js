cfe63e1918ade6c8020135f5a6cec080be6309dc
145a <133004095+54145a@users.noreply.github.com>
2023-10-04 12:15:13 +0800

    kk

--- _worker.js
+++ _worker.js
@@ -1,32 +1,8 @@
-
-class AttributeRewriter {
-  constructor(attributeName) {
-    this.attributeName = attributeName;
-  }
-
-  element(element) {
-    const attribute = element.getAttribute(this.attributeName);
-    if (attribute) {
-      element.setAttribute(
-        this.attributeName,
-        attribute.replace('//github.com', '//145atest2.pages.dev')
-      );
-    }
-  }
-}
-const rewriter = new HTMLRewriter()
-  .on('a', new AttributeRewriter('href'))
-  .on('img', new AttributeRewriter('src'));
 export default {
   async fetch(request, env) {
-    try {
       let url = new URL(request.url);
       url.hostname = "github.com";
       let new_request = new Request(url, request);
-      let new_res = fetch(new_request);
-      return rewriter.transform(new_res);
-    } catch (e) {
-      return e;
-    }
+      return fetch(new_request);
   }
 };
