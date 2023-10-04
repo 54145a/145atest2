603c4da5b27fb16724b221f1261b81dcde9f4c37
145a <133004095+54145a@users.noreply.github.com>
2023-10-04 11:56:28 +0800

    a

--- _worker.js
+++ _worker.js
@@ -1,9 +1,28 @@
+const rewriter = new HTMLRewriter()
+  .on('a', new AttributeRewriter('href'))
+  .on('img', new AttributeRewriter('src'));
+
+class AttributeRewriter {
+  constructor(attributeName) {
+    this.attributeName = attributeName;
+  }
+
+  element(element) {
+    const attribute = element.getAttribute(this.attributeName);
+    if (attribute) {
+      element.setAttribute(
+        this.attributeName,
+        attribute.replace('//github.com', '//145atest2.pages.dev')
+      );
+    }
+  }
+}
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     url.hostname = "github.com";
     let new_request = new Request(url, request);
-    new_request.redirect = "follow";
-    return fetch(new_request);
+    let new_res = fetch(new_request);
+    return rewriter.transform(new_res)
   }
 };
