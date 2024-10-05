7062f97d748da2ad93606709ba7be654b120db0f
145a <133004095+54145a@users.noreply.github.com>
2024-10-05 22:55:57 +0800

    headers

--- _worker.js
+++ _worker.js
@@ -1,11 +1,15 @@
+let targetHostname = "zh.wikipedia.org";
 export default {
     async fetch(request, env) {
         let url = new URL(request.url);
         if (url.pathname === "/robots.txt") {
             return new Response("User-Agent: *\nDisallow: /");
         }
-        url.hostname = "zh.wikipedia.org";
-        let new_request = new Request(url, request);
-        return fetch(new_request);
+        url.hostname = targetHostname;
+        let newRequest = new Request(url, request);
+        newRequest.headers.set("Access-Control-Allow-Origin", "*");
+        newRequest.headers.delete("Origin");
+        newRequest.headers.delete("Referrer");
+        return fetch(newRequest);
     }
 };
